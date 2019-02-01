d0337288f25d9b8605bdb3a0699d981c5a80f0fc
Olamide <aboyejiolamide15@gmail.com>
2019-02-01 20:28:11 +0100

    Updates

---
+++ precache-manifest.00f40a6248e06fd377251d07888629b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2af48a37d07ec98825d",
+    "url": "/movie-search-app/static/js/runtime~main.b2af48a3.js"
+  },
+  {
+    "revision": "de72e2d015c1e8554d00",
+    "url": "/movie-search-app/static/js/main.de72e2d0.chunk.js"
+  },
+  {
+    "revision": "ec5d15567dc43003ccaa",
+    "url": "/movie-search-app/static/js/1.ec5d1556.chunk.js"
+  },
+  {
+    "revision": "de72e2d015c1e8554d00",
+    "url": "/movie-search-app/static/css/main.afcf7225.chunk.css"
+  },
+  {
+    "revision": "c2691daa0e027cac68419aacc08dfc19",
+    "url": "/movie-search-app/index.html"
+  }
+];
