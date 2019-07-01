637a778a935db08b2a5216be6175706f3e4e28db
Olamide <aboyejiolamide15@gmail.com>
2019-07-02 00:46:03 +0100

    Updates

--- precache-manifest.4af1e907cd37f6c1577d8a3a3b5892f6.js
+++ precache-manifest.4af1e907cd37f6c1577d8a3a3b5892f6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/movie-search-app/static/js/runtime~main.b2af48a3.js"
   },
   {
-    "revision": "4bac7c4b570a20ac23bf",
-    "url": "/movie-search-app/static/js/main.4bac7c4b.chunk.js"
+    "revision": "4d97876506017172b5f2",
+    "url": "/movie-search-app/static/js/main.4d978765.chunk.js"
   },
   {
     "revision": "ec5d15567dc43003ccaa",
     "url": "/movie-search-app/static/js/1.ec5d1556.chunk.js"
   },
   {
-    "revision": "4bac7c4b570a20ac23bf",
-    "url": "/movie-search-app/static/css/main.fede01b3.chunk.css"
+    "revision": "4d97876506017172b5f2",
+    "url": "/movie-search-app/static/css/main.1bf452ac.chunk.css"
   },
   {
-    "revision": "f8df3adbc69700cb196cb7fabe685df2",
+    "revision": "4a1f02f4a65531d5f04133dbc24d5d7c",
     "url": "/movie-search-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-search-app/precache-manifest.2a69e1d2cf247a69cab4f6da910bf579.js"
+  "/movie-search-app/precache-manifest.4af1e907cd37f6c1577d8a3a3b5892f6.js"
 );
 
 workbox.clientsClaim();
