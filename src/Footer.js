18fdaee30168d2fbc9895e81ba002b36b38da075
Olamide <aboyejiolamide15@gmail.com>
2019-07-10 00:30:11 +0100

    fixed linkedin link

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
        <div style={{textAlign : 'center', background: 'orange'}}>
            <p>Made with <i className="fa fa-heart"></i> by Aboyeji Olamide</p>
            <div>
-                <a href="https://linkedin.com/in/aolmide" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin fa-2x"></i></a>
+                <a href="https://linkedin.com/in/aolamide" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin fa-2x"></i></a>
                 <a href="https://github.com/aolamide" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-2x"></i></a>
                 <a href="mailto:aboyejiolamide15@gmail.com" target="_blank" rel="noopener noreferrer"><i className="fa fa-envelope fa-2x"></i></a>
            </div>
